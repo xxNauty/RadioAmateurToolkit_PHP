5e11b5b1f0178a7f4d0b3363e46875b622bdca64
Mateusz Wnuk <mateusz2003w@gmail.com>
2025-05-02 14:52:46 +0200

    nato phonetic alphabet

---
+++ src/Repository/NATOPhoneticLetterRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\NATOPhoneticLetter;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<NATOPhoneticLetter>
+ */
+class NATOPhoneticLetterRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, NATOPhoneticLetter::class);
+    }
+
+    //    /**
+    //     * @return NATOPhoneticLetter[] Returns an array of NATOPhoneticLetter objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('n')
+    //            ->andWhere('n.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('n.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?NATOPhoneticLetter
+    //    {
+    //        return $this->createQueryBuilder('n')
+    //            ->andWhere('n.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
