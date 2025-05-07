48aad0e6f86378c0393147275236bacb16db8744
Mateusz Wnuk <mateusz2003w@gmail.com>
2025-05-07 14:21:17 +0200

    migrations fixed, standarized look of migrations, renamed table exam_question to polish_exam_question

--- migrations/Version20250502130210.php
+++ migrations/Version20250502130210.php
@@ -7,27 +7,31 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20250502130210 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Table for Polish phonetic alphabet.';
+        return
+            '
+                1. Table for Polish phonetic alphabet.
+            ';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            CREATE TABLE polish_phonetic_letter (id INT AUTO_INCREMENT NOT NULL, letter VARCHAR(1) NOT NULL, code_word VARCHAR(14) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+            CREATE TABLE polish_phonetic_letter 
+            (
+                id INT AUTO_INCREMENT NOT NULL, 
+                letter VARCHAR(1) NOT NULL, 
+                code_word VARCHAR(14) NOT NULL, 
+                PRIMARY KEY(id)
+            ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
             DROP TABLE polish_phonetic_letter
         SQL);
--- migrations/Version20250505130223.php
+++ migrations/Version20250505130223.php
@@ -7,27 +7,31 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20250505130223 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Table for English version of q-code';
+        return
+            '
+                1. Table for English version of q-code
+            ';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            CREATE TABLE english_qcode (id INT AUTO_INCREMENT NOT NULL, code VARCHAR(4) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+            CREATE TABLE english_qcode 
+            (
+                id INT AUTO_INCREMENT NOT NULL, 
+                code VARCHAR(4) NOT NULL, 
+                description VARCHAR(255) NOT NULL, 
+                PRIMARY KEY(id)
+            ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
             DROP TABLE english_qcode
         SQL);
